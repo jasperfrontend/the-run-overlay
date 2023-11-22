7c313acaac3dfc54228d922dcafa9a7c97c5a785
Jasper van der Meer <jaspervdmeer@gmail.com>
2023-11-22 21:13:52 +0100

    Beiroet

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,52 @@
 <template>
   <div class="container">
-
-    <div v-if="posts.acf.player_1_winner" class="winner winner_p1">
-      <div class="overlay">
-        <div class="text">
-          <Transition>
-            <div class="name" v-if="posts.acf.player_1_name">{{ posts.acf.player_1_name }}</div>
-            <div class="name" v-else>Loading name&hellip;</div>
-          </Transition>
-          <Transition>
+    <span v-show="posts && posts.acf && posts.acf.player_1_winner === true">
+      <div class="winner winner_p1">
+        <div class="overlay">
+          <div class="text">
+            <Transition>
+              <div class="name" v-if="posts && posts.acf && posts.acf.player_1_name">{{ posts.acf.player_1_name }}</div>
+              <div class="name" v-else>Player 1</div>
+            </Transition>
+            <Transition>
+              <div class="body">
+                is the winner!
+              </div> <!-- .body -->
+            </Transition>
+          </div> <!-- .text -->
+        </div> <!-- .overlay -->
+      </div> <!-- .winner -->
+    </span>
+    <span v-show="posts && posts.acf && posts.acf.player_2_winner === true">
+      <div class="winner winner_p2">
+        <div class="overlay">
+          <div class="text">
+            <div class="name" v-if="posts && posts.acf && posts.acf.player_2_name">{{ posts.acf.player_2_name }}</div>
+            <div class="name" v-else>Player 2</div>
             <div class="body">
               is the winner!
             </div> <!-- .body -->
-          </Transition>
-        </div> <!-- .text -->
-      </div> <!-- .overlay -->
-    </div> <!-- .winner -->
-    
-    <div v-if="posts.acf.player_2_winner" class="winner winner_p2">
-      <div class="overlay">
-        <div class="text">
-          <div class="name" v-if="posts.acf.player_2_name">{{ posts.acf.player_2_name }}</div>
-          <div class="name" v-else>Loading name&hellip;</div>
-          <div class="body">
-            is the winner!
-          </div> <!-- .body -->
-        </div> <!-- .text -->
-      </div> <!-- .overlay -->
-    </div> <!-- .winner -->
-
-    <div class="fireworks" v-if="posts.acf.player_1_winner || posts.acf.player_2_winner">
-      <video autoplay muted loop>
-        <source src="https://host.run.city/wp-content/uploads/2023/11/twinkle.webm" type="video/webm">
-      </video>
-    </div>
-
+          </div> <!-- .text -->
+        </div> <!-- .overlay -->
+      </div> <!-- .winner -->
+    </span>
+    <span v-show="posts && posts.acf && posts.acf.player_1_winner || posts && posts.acf && posts.acf.player_2_winner">
+      <div class="fireworks">
+        <video autoplay muted loop>
+          <source src="https://host.run.city/wp-content/uploads/2023/11/twinkle.webm" type="video/webm">
+        </video>
+      </div>
+    </span>
     <div class="bar">
       <div class="bar-top">
         <div class="col col-player-name col-player-one">
-          <h2 v-if="posts.acf.player_1_name">{{ posts.acf.player_1_name }}</h2>
-          <h2 v-else="posts.acf.player_1_name">Loading name&hellip;</h2>
+          <h2 v-if="posts && posts.acf && posts.acf.player_1_name">{{ posts.acf.player_1_name }}</h2>
         </div>
         <div class="col col-hints">
 
         </div>
         <div class="col col-player-name col-player-two">
-          <h2 v-if="posts.acf.player_2_name">{{ posts.acf.player_2_name }}</h2>
-          <h2 v-else="posts.acf.player_2_name">Loading name&hellip;</h2>
+          <h2 v-if="posts && posts.acf && posts.acf.player_2_name">{{ posts.acf.player_2_name }}</h2>
         </div>
       </div> <!-- .bar-top -->
       <div class="bar-bottom">
@@ -54,74 +54,74 @@
           <div class="flex row justify-content-space-between player-one-gates">
             <div class="gate gate-1">
               <Transition>
-                <img :class="posts.acf.gates_p1.gate_1_player_1 ? 'icon' : 'icon disabled'" src="./assets/Gates/G1.png" width="60" height="60" alt="G1">
+                <img :class="posts && posts.acf && posts.acf.gates_p1.gate_1_player_1 ? 'icon' : 'icon disabled'" src="./assets/Gates/G1.png" width="60" height="60" alt="G1">
               </Transition>
             </div>
             <div class="gate gate-2">
               <Transition>
-                <img :class="posts.acf.gates_p1.gate_2_player_1 ? 'icon' : 'icon disabled'" src="./assets/Gates/G2.png" width="60" height="60" alt="G2">
+                <img :class="posts && posts.acf && posts.acf.gates_p1.gate_2_player_1 ? 'icon' : 'icon disabled'" src="./assets/Gates/G2.png" width="60" height="60" alt="G2">
               </Transition>
             </div>
             <div class="gate gate-3">
               <Transition>
-                <img :class="posts.acf.gates_p1.gate_3_player_1 ? 'icon' : 'icon disabled'" src="./assets/Gates/G3.png" width="60" height="60" alt="G3">
+                <img :class="posts && posts.acf && posts.acf.gates_p1.gate_3_player_1 ? 'icon' : 'icon disabled'" src="./assets/Gates/G3.png" width="60" height="60" alt="G3">
               </Transition>
             </div>
           </div>
           <div class="flex row justify-content-space-between player-one landmarks">
             <div class="landmark landmark-1">
               <Transition>
-                <img :class="posts.acf.lm_p1.landmark_1 ? 'icon' : 'icon disabled'" src="./assets/Landmarks/LM1.png" width="60" height="60" alt="LM1">
+                <img :class="posts && posts.acf && posts.acf.lm_p1.landmark_1 ? 'icon' : 'icon disabled'" src="./assets/Landmarks/LM1.png" width="60" height="60" alt="LM1">
               </Transition>
             </div>
             <div class="landmark landmark-2">
               <Transition>
-                <img :class="posts.acf.lm_p1.landmark_2 ? 'icon' : 'icon disabled'" src="./assets/Landmarks/LM2.png" width="60" height="60" alt="LM2">
+                <img :class="posts && posts.acf && posts.acf.lm_p1.landmark_2 ? 'icon' : 'icon disabled'" src="./assets/Landmarks/LM2.png" width="60" height="60" alt="LM2">
               </Transition>
             </div>
             <div class="landmark landmark-3">
               <Transition>
-                <img :class="posts.acf.lm_p1.landmark_3 ? 'icon' : 'icon disabled'" src="./assets/Landmarks/LM3.png" width="60" height="60" alt="LM3">
+                <img :class="posts && posts.acf && posts.acf.lm_p1.landmark_3 ? 'icon' : 'icon disabled'" src="./assets/Landmarks/LM3.png" width="60" height="60" alt="LM3">
               </Transition>
             </div>
           </div>
         </div> <!-- .col-player-one-icons -->
         <div class="flex row justify-content-space-between col col-gates">
-          <p v-if="posts.acf.hint">{{ posts.acf.hint }}</p>
-          <p v-else="posts.acf.hint">Loading&hellip;</p>
+          <p v-if="posts && posts.acf && posts.acf.hint">{{ posts.acf.hint }}</p>
+          <p v-else></p>
         </div>
         <div class="flex row col col-player-icons col-player-two-icons">
           <div class="flex row justify-content-space-between player-two landmarks">
             <div class="landmark-6">
               <Transition>
-                <img :class="posts.acf.lm_p2.landmark_6 ? 'icon' : 'icon disabled'" src="./assets/Landmarks/LM6.png" width="60" height="60" alt="LM6">
+                <img :class="posts && posts.acf && posts.acf.lm_p2.landmark_6 ? 'icon' : 'icon disabled'" src="./assets/Landmarks/LM6.png" width="60" height="60" alt="LM6">
               </Transition>
             </div>
             <div class="landmark-5">
               <Transition>
-                <img :class="posts.acf.lm_p2.landmark_5 ? 'icon' : 'icon disabled'" src="./assets/Landmarks/LM5.png" width="60" height="60" alt="LM5">
+                <img :class="posts && posts.acf && posts.acf.lm_p2.landmark_5 ? 'icon' : 'icon disabled'" src="./assets/Landmarks/LM5.png" width="60" height="60" alt="LM5">
               </Transition>
             </div>
             <div class="landmark-4">
               <Transition>
-                <img :class="posts.acf.lm_p2.landmark_4 ? 'icon' : 'icon disabled'" src="./assets/Landmarks/LM4.png" width="60" height="60" alt="LM4">
+                <img :class="posts && posts.acf && posts.acf.lm_p2.landmark_4 ? 'icon' : 'icon disabled'" src="./assets/Landmarks/LM4.png" width="60" height="60" alt="LM4">
               </Transition>
             </div>
           </div>
           <div class="flex row justify-content-space-between player-two-gates">
             <div class="gate gate-3">
               <Transition>
-                <img :class="posts.acf.gates_p2.gate_3_player_2 ? 'icon' : 'icon disabled'" src="./assets/Gates/G3.png" width="60" height="60" alt="G3">
+                <img :class="posts && posts.acf && posts.acf.gates_p2.gate_3_player_2 ? 'icon' : 'icon disabled'" src="./assets/Gates/G3.png" width="60" height="60" alt="G3">
               </Transition>
             </div>
             <div class="gate gate-2">
               <Transition>
-                <img :class="posts.acf.gates_p2.gate_2_player_2 ? 'icon' : 'icon disabled'" src="./assets/Gates/G2.png" width="60" height="60" alt="G2">
+                <img :class="posts && posts.acf && posts.acf.gates_p2.gate_2_player_2 ? 'icon' : 'icon disabled'" src="./assets/Gates/G2.png" width="60" height="60" alt="G2">
               </Transition>
             </div>
             <div class="gate gate-1">
               <Transition>
-                <img :class="posts.acf.gates_p2.gate_1_player_2 ? 'icon' : 'icon disabled'" src="./assets/Gates/G1.png" width="60" height="60" alt="G1">
+                <img :class="posts && posts.acf.gates_p2.gate_1_player_2 ? 'icon' : 'icon disabled'" src="./assets/Gates/G1.png" width="60" height="60" alt="G1">
               </Transition>
             </div>
           </div>
@@ -136,7 +136,6 @@ export default {
   data() {
     return {
       posts: null,
-      avatars: null,
     };
   },
   created() {
@@ -145,8 +144,8 @@ export default {
   },
   methods: {
     async fetchWordPressData() {
-      const response = await fetch('https://host.run.city/wp-json/wp/v2/pages/2');
-      this.posts = await response.json();
+      const wpData = await fetch('https://host.run.city/wp-json/wp/v2/pages/2');
+      this.posts = await wpData.json();
     },
   },
 };
